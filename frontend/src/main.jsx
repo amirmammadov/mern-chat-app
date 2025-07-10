e238ec789ae6b0ff722dbe7f8152d20b6aae5f85
Amir Mammadov <amir.mammadov@simple.co>
2025-07-10 12:57:12 +0400

    login, signup pages and navbar completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+import App from "./App.jsx";
+
+import { Toaster } from "react-hot-toast";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+      <Toaster position="top-right" reverseOrder={false} />
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import SettingsPage from "./SettingsPage";
+import LoginPage from "./LoginPage";
+import SignUpPage from "./SignUpPage";
+import ProfilePage from "./ProfilePage";
+
+export { HomePage, SettingsPage, LoginPage, SignUpPage, ProfilePage };
