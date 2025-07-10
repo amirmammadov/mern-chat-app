a60d7e740c7a8fe148e5caa1855886126cc33d06
Amir Mammadov <amir.mammadov@simple.co>
2025-07-10 12:58:19 +0400

    cors config added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "../src/routes/auth.route.js";
 import messageRoutes from "../src/routes/message.route.js";
@@ -14,6 +15,13 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
